f3fd0172fb3540f32cf691468ef37183cd78291d
Ron Hashimoto <mail@h2so5.net>
2018-11-03 18:50:08 +0900

    bit

--- genet-kernel/examples/eth.rs
+++ genet-kernel/examples/eth.rs
@@ -23,7 +23,18 @@ struct Eth {
 
     /// Protocol Type
     #[genet(typ = "@enum")]
-    r#type: Detach<Node<cast::UInt16BE, EthType>>,
+    r#type: Node<cast::UInt16BE, EthType>,
+
+    #[genet(bit_size = 1)]
+    n1_ipv4_addr: cast::UInt8,
+
+    #[genet(bit_size = 1)]
+    n2: cast::UInt8,
+
+    #[genet(bit_size = 6)]
+    n3: cast::UInt8,
+
+    n4: cast::UInt8,
 }
 
 type DUInt8 = Detach<cast::UInt8>;
@@ -61,7 +72,7 @@ impl Worker for EthWorker {
                 let payload = parent.data().try_get(14..)?;
                 layer.add_payload(Payload::new(payload, typ));
             }
-            layer.add_attr(attr!(self.eth.attr().r#type.as_ref().clone(), range: 12..14));
+            // layer.add_attr(attr!(self.eth.attr().r#type.as_ref().clone(), range: 12..14));
             parent.add_child(layer);
             Ok(Status::Done)
         } else {
